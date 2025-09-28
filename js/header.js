fe2bb008ef696e30c08d4f09826b9f52205eaf42
Jan Vidar <jan_94_@hotmail.com>
2025-09-28 22:09:41 +0200

    test

--- js/header.js
+++ js/header.js
@@ -1,14 +1,14 @@
 let logo = document.querySelectorAll(".logo");
 for(let i = 0; i < logo.length; i++) {
         logo[i].addEventListener("click", function(){
-            location.href = "../index.html";
+            location.href = ".../index.html";
     });
 }
 
 let user = document.querySelectorAll(".user");
 for(let i = 0; i < user.length; i++) {
         user[i].addEventListener("click", function(){
-            location.href = "login.html";
+            location.href = ".login.html";
     });
 }
 
